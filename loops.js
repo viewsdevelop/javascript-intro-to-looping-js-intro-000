4aa09b344ae41f38a165e47bb1cd4c88313314a5
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-11 00:46:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,5 +17,14 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(n) {
-  
+  let i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+    return i;
+  }
+   
+  do {
+    console.log("doo-bee-doo-bee-doo");
+  } while (incrementVariable() < 5);
 }
