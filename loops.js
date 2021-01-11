a4287f10070868bb5c194866bb467b4d4e844b2d
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-11 00:34:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i == 1) {
-      array.push("I am #{i} strange loop!")
+    if (i == 0) {
+      array.push("I am #{i} strange loop.")
+    } else {
+      array.push
     }
   }
 }
